07013755b9161c88c41429d9c5d8aeaa93699e9e
ngnadeera <79752484+ngnadeera@users.noreply.github.com>
2021-10-02 15:51:55 +0530

    Add files via upload

---
+++ Homework 7/Homework 7.5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string.h>
+
+
+using namespace std;
+
+int main ()
+{
+  char s[100];   //hold the string
+  cout << "Enter the sentence : ";
+  gets(s);      // getting the user_input 
+  
+  char *token;         //  hold the separator value
+  token = strtok(s,"\n "); //seperate the text by the spaces 
+  int count = 0;      //to hold the count 
+  while(token != NULL){     //
+  
+  	count ++;
+  	token = strtok(NULL,"\n ");
+  	
+  }
+  cout << "There weere " << count << " words in that sentence ::";
+  return 0;
+}
